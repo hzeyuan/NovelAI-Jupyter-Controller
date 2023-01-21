85ec423bfd137a21670ab47e9e7cbc4767aa9684
xiaolxl <481719882@qq.com>
2023-01-21 10:20:29 +0800

    update update

--- ui-script/UpdataUi.py
+++ ui-script/UpdataUi.py
@@ -1,5 +1,5 @@
 import ipywidgets as widgets
-from ipywidgets import Layout,Label, HBox, VBox
+from ipywidgets import Layout,Label, HBox, VBox,Button
 import os
 import subprocess
 
@@ -9,6 +9,81 @@ chinese_name = {
     "stable-diffusion-webui-localization-zh_CN":"汉化插件",
     "sd_dreambooth_extension":"Dreambooth插件"
 }
+
+class Git_Item:
+    def __init__(self,path,name,out):
+        sd_dir = Utils.get_sd_dir()
+        self.path = sd_dir + path
+        self.out = out
+        
+        self.refresh_button = Button(description = name + '(点我刷新)',layout=Layout(width='auto', height='auto'),button_style='info')
+        self.refresh_button.on_click(self.refresh)
+        
+        self.now_b_name = Label(value="当前版本分支：请先刷新")
+        self.now_v_sha = Label(value="当前版本SHA：请先刷新")
+        self.left_box = VBox([self.now_b_name, self.now_v_sha])
+
+        self.bb = widgets.Dropdown(
+            options=[('请先刷新', '请先刷新')],
+            value='请先刷新',
+            description='分支选择:',
+        )
+        self.sha = widgets.Text(
+            value='',
+            placeholder='不填默认更新为最新',
+            description='自定义版本SHA值:',
+            style={'description_width': 'initial'},
+            layout=Layout(width='auto', height='auto'),
+            disabled=False
+        )
+        self.right_box = VBox([self.bb, self.sha])
+        
+        self.change_button = Button(description='设置插件版本',layout=Layout(width='150px', height='auto'),button_style='success')
+        self.change_button.on_click(self.click)
+        
+        self.Box = HBox(children=[self.refresh_button,
+                                  self.left_box,
+                                  self.right_box,
+                                  self.change_button],
+                        layout=Layout(border='solid 1px',width='100%'))
+    def click(self,temp):
+        self.change_button.description = "正在更新..."
+        self.change_button.button_style = "warning"
+        
+        self.out.clear_output()
+        with self.out:
+            Utils.change_b_and_v(self.path,self.bb.value,self.sha.value)
+            self.refresh(self)
+            print("更新成功！")
+            self.change_button.description = "设置插件版本"
+            self.change_button.button_style = "success"
+        
+    def get_box(self):
+        return self.Box
+    
+    def refresh(self,temp):
+        old_des = self.refresh_button.description
+        self.refresh_button.description = "正在检查..."
+        self.refresh_button.button_style = "warning"
+        
+        self.out.clear_output()
+        with self.out:
+            b_now_name = Utils.get_nov_b_name(self.path)
+            b_main_name = Utils.get_main_b_name(self.path)
+            self.now_b_name.value = "当前版本分支：" + b_now_name
+            self.now_v_sha.value = "当前版本SHA：" + Utils.get_now_v_sha(self.path)
+            data = Utils.get_all_b_name(self.path)
+            temp_arr = []
+            for item in data:
+                if b_main_name == item['branch_name']:
+                    temp_arr.append((item['branch_name']+"(主分支)",item['branch_name']))
+                else:
+                    temp_arr.append((item['branch_name'],item['branch_name']))
+            self.bb.options = temp_arr
+            self.bb.value = b_now_name
+            
+            self.refresh_button.description = old_des
+            self.refresh_button.button_style = "info"
 
 def getUi(data,cmd_run):
     out = widgets.Output(layout={'border': '1px solid black'})
@@ -26,15 +101,17 @@ def getUi(data,cmd_run):
             button_style='primary'
     )
     
-    updata_webui_buttom = widgets.Button(
-            description='更新WebUi(注意这是强制覆盖)',
-            style={'description_width': 'initial'},
-            layout=Layout(width='300px', height='auto'),
-            button_style='success'
-    )
+    webui_Item = Git_Item('/','WebUi',out)
+    
+    # updata_webui_buttom = widgets.Button(
+    #         description='更新WebUi(注意这是强制覆盖)',
+    #         style={'description_width': 'initial'},
+    #         layout=Layout(width='300px', height='auto'),
+    #         button_style='success'
+    # )
     
     updata_scan = widgets.Button(
-            description='扫描可更新程序与插件(记得开学术加速)[绿色代表已最新，黄色代表可更新]',
+            description='扫描可更新程序与插件(记得开学术加速)[设置版本前一定要先刷新！]',
             style={'description_width': 'initial'},
             layout=Layout(width='600px', height='auto'),
             button_style='info'
@@ -66,12 +143,12 @@ def getUi(data,cmd_run):
     def updata_controller_buttom_(self):
         run_click(0)   
     
-    def updata_webui_buttom_(self):
-        run_click(1)
+    # def updata_webui_buttom_(self):
+    #     run_click(1)
     
     #绑定加速函数
     updata_controller_buttom.on_click(updata_controller_buttom_)
-    updata_webui_buttom.on_click(updata_webui_buttom_)
+    # updata_webui_buttom.on_click(updata_webui_buttom_)
 
     # ====================
 
@@ -132,31 +209,41 @@ def getUi(data,cmd_run):
         extension_list = scan_extension_dir()
         button_list = [] # 创建一个空的button列表，用于存储按钮对象    
         for extension in extension_list: 
-            # 判断当前插件是否需要更新    
-            if is_need_update(extension['path']): 
-                # 如果需要更新，则创建一个黄色的按钮    
-                if extension.get('chinese_name') != "":
-                    btn = widgets.Button(description = extension['chinese_name'], button_style='warning',layout=Layout(width='400px', height='auto')) 
-                else:
-                    btn = widgets.Button(description = extension['name'], button_style='warning',layout=Layout(width='400px', height='auto')) 
+            
+            if extension.get('chinese_name') != "":
+                webui_Item_ = Git_Item('/extensions/' + extension['name'],extension['chinese_name'],out)
+            else:
+                webui_Item_ = Git_Item('/extensions/' + extension['name'],extension['name'],out)
+            
+            button_list.append(webui_Item_.get_box())# 将创建的按钮对象存储到button列表中  
+            
+#             # 判断当前插件是否需要更新  
+#             if is_need_update(extension['path']): 
+#                 # 如果需要更新，则创建一个黄色的按钮    
+#                 if extension.get('chinese_name') != "":
+#                     btn = widgets.Button(description = extension['chinese_name'], button_style='warning',layout=Layout(width='400px', height='auto')) 
+#                 else:
+#                     btn = widgets.Button(description = extension['name'], button_style='warning',layout=Layout(width='400px', height='auto')) 
 
-                # 给按钮绑定一个事件处理函数，当点击该按钮时，调用update_extension函数进行更新    
-                btn.on_click(lambda btn: update_extension(btn.description,btn))
-                button_list.append(btn)# 将创建的按钮对象存储到button列表中  
-            else: 
-                # 否则创建一个绿色的按钮    
-                if extension.get('chinese_name') != "":
-                    btn = widgets.Button(description = extension['chinese_name'], button_style='success',layout=Layout(width='400px', height='auto'))
-                else:
-                    btn = widgets.Button(description = extension['name'], button_style='success',layout=Layout(width='400px', height='auto'))
-                btn.on_click(lambda btn: update_extension(btn.description,btn))
-                button_list.append(btn)# 将创建的按钮对象存储到button列表中  
+#                 # 给按钮绑定一个事件处理函数，当点击该按钮时，调用update_extension函数进行更新    
+#                 btn.on_click(lambda btn: update_extension(btn.description,btn))
+#                 button_list.append(btn)# 将创建的按钮对象存储到button列表中
+#             else: 
+#                 # 否则创建一个绿色的按钮
+#                 if extension.get('chinese_name') != "":
+#                     btn = widgets.Button(description = extension['chinese_name'], button_style='success',layout=Layout(width='400px', height='auto'))
+#                 else:
+#                     btn = widgets.Button(description = extension['name'], button_style='success',layout=Layout(width='400px', height='auto'))
+#                 btn.on_click(lambda btn: update_extension(btn.description,btn))
+#                 button_list.append(btn)# 将创建的按钮对象存储到button列表中  
+                
+                
         # 显示button列表中的所有按钮
-        box_.children = [updata_tip,updata_controller_buttom,updata_webui_buttom,updata_scan,updata_line,VBox(button_list),out]
+        box_.children = [updata_tip,updata_controller_buttom,webui_Item.get_box(),updata_scan,updata_line,VBox(button_list),out]
         self.description = "扫描可更新程序与插件(记得开学术加速)[绿色代表已最新，黄色代表可更新]"
         self.button_style = "info"
         
     updata_scan.on_click(scan_run)
-        
-    box_ = VBox([updata_tip,updata_controller_buttom,updata_webui_buttom,updata_scan,updata_line,out])
+    
+    box_ = VBox([updata_tip,updata_controller_buttom,webui_Item.get_box(),updata_scan,updata_line,out])
     return box_
--- ui-script/Utils.py
+++ ui-script/Utils.py
@@ -1,6 +1,8 @@
 import os
 import random
 import hashlib
+import subprocess
+import time
 
 ip_list = {
     '芜湖区':'192.168.0.91',
@@ -104,3 +106,113 @@ def scan_dir_hash(style):
         if os.path.isfile(file_dir):
             hash_list.append(model_hash(file_dir))
     return hash_list
+
+# 获取默认分支名字
+def get_main_b_name(path):
+    output = subprocess.check_output(
+        ['git', 'ls-remote', '--symref', 'origin', 'HEAD'],
+        cwd=path
+    ).strip().decode('utf-8')
+
+    # Get the branch name from the output
+    return  output.split('/')[-1].split('\t')[0]
+
+# 获取全部分支名字
+def get_all_b_name(path):
+    data = []
+    output = subprocess.check_output(
+        ['git', 'ls-remote', '--heads', 'origin'],
+        cwd=path
+    ).strip().decode('utf-8')
+
+    main_b_name = get_main_b_name(path)
+    
+    for line in output.split('\n'):
+        branch_name = line.split('/')[-1]
+        
+        if main_b_name == branch_name:
+            item = {'branch_name':branch_name,'is_main':1}
+        else:
+            item = {'branch_name':branch_name,'is_main':0}
+        
+        data.append(item)
+    
+    return data
+
+# 获取当前分支名字
+def get_nov_b_name(path):
+    return subprocess.check_output(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD@{upstream}'],
+        cwd=path
+    ).strip().decode('utf-8').split('/')[1]
+
+# 获取当前分支SHA
+def get_now_v_sha(path):
+    output = subprocess.check_output(
+        ['git', 'rev-parse', 'HEAD'],
+        cwd=path
+    ).strip()
+
+    # Convert the output to a string
+    output = output.decode('utf-8')
+
+    # Remove the extra content
+    full_sha = output.split('/')[0]
+    
+    return full_sha
+
+# 切换分支与版本
+def change_b_and_v(path,branch_name,full_sha):
+    git_path = path
+
+    if full_sha != "":
+        # Fetch latest remote changes
+        subprocess.run(
+            ['git', 'fetch', '--all'],
+            cwd=git_path
+        )
+
+        # Checkout the specified branch
+        # subprocess.run(
+        #     ['git', 'checkout','-b', branch_name],
+        #     cwd=git_path
+        # )
+
+        # Update Branch
+        subprocess.run(
+            ['git', 'pull'],
+            cwd=git_path
+        )
+
+        # Reset to the specified version
+        subprocess.run(
+            ['git', 'reset', '--hard', full_sha],
+            cwd=git_path
+        )
+        
+        # Update Branch
+        # subprocess.run(
+        #     ['git', 'pull'],
+        #     cwd=git_path
+        # )
+    else:
+        # Fetch latest remote changes
+        subprocess.run(
+            ['git', 'fetch', '--all'],
+            cwd=git_path
+        )
+        
+        # Checkout the specified branch
+        subprocess.run(
+            ['git', 'branch','--set-upstream-to=origin/' + branch_name, 'HEAD'],
+            cwd=git_path
+        )
+        
+        subprocess.run(
+            ['git', 'reset', '--hard', 'origin/' + branch_name],
+            cwd=git_path
+        )
+        subprocess.run(
+            ['git', 'pull'],
+            cwd=git_path
+        )
